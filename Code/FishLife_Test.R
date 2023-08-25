8b82e9af2bfc5aa6c1848ea32961f195b92d51dd
Louw-C <kyss.louw@gmail.com>
2023-08-25 08:54:05 +0200

    Testing different code for this package - no idea what it means though?!

--- Code/FishLife_Test.R
+++ Code/FishLife_Test.R
@@ -1,2 +1,9 @@
+Require(devtools)
+# Install and load package
+devtools::install_github("james-thorson/FishLife")
+library( FishLife )
+
+vignette("tutorial","FishLife")
+
 # Get basic plot for Lutjanus campechanus (in database, so prediction is informed by species-specific data)
 Plot_taxa( Search_species(Genus="Lutjanus",Species="campechanus")$match_taxonomy )
