dd26a6e721f016985007fc02956a3f9d85fc5d28
Louw-C <kyss.louw@gmail.com>
2023-08-25 08:40:22 +0200

    Testing out FishLife to get life history info for different taxa

---
+++ Code/FishLife_Test.R
@@ -0,0 +1,2 @@
+# Get basic plot for Lutjanus campechanus (in database, so prediction is informed by species-specific data)
+Plot_taxa( Search_species(Genus="Lutjanus",Species="campechanus")$match_taxonomy )
